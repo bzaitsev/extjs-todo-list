ebac7759ccb5249c08eaf60ac6e093817550c71a
boris_zaytsev <bizaytsev@gmail.com>
2022-08-30 17:53:08 +0300

    trash

--- build/temp/development/MyApp/sass/config.rb
+++
@@ -1,2 +0,0 @@
-Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
-cache_path = 'D:\local projects\rest\ExtTodo/build/.sass-cache'
--- build/temp/development/MyApp/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require D:\local projects\rest\ExtTodo\app.js
